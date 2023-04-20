e1a6208807e0b03d8179fe798fd87d7a56ef57b0
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-20 17:39:39 +0600

    add footer to app and give styles.
    add footer component to the app.. the span is kept as a dummy text. added arbitrary values to  to the grid cols and rows.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="grid h-screen w-screen grid-cols-[1fr_3fr] grid-rows-[80px_1fr_50px]">
       <Header />
+      <span className="w-4 bg-black">Something</span>
+      <span className="w-4 bg-black">Something</span>
+      <Footer />
     </div>
   );
 }
