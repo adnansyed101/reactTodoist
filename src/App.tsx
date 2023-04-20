f4a42f760df5ec59ddfd54ec3f407982da447ccc
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-20 17:41:09 +0600

    update height of footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="grid h-screen w-screen grid-cols-[1fr_3fr] grid-rows-[80px_1fr_50px]">
+    <div className="grid h-screen w-screen grid-cols-[1fr_3fr] grid-rows-[80px_1fr_35px]">
       <Header />
       <span className="w-4 bg-black">Something</span>
       <span className="w-4 bg-black">Something</span>
