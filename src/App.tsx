44f1e3b887a8dbcbc867851d34ac0a6fa680f249
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-20 19:53:57 +0600

    add Sidebar and main component
    Also increase the size of the main component by increase grid col size

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="grid h-screen w-screen grid-cols-[1fr_3fr] grid-rows-[80px_1fr_35px]">
+    <div className="grid h-screen w-screen grid-cols-[1fr_4fr] grid-rows-[80px_1fr_35px]">
       <Header />
-      <span className="w-4 bg-black">Something</span>
-      <span className="w-4 bg-black">Something</span>
+      <Sidebar />
+      <Main />
       <Footer />
     </div>
   );
