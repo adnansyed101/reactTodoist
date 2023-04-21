0340ddf7121f5877aeae06971fcf56c21484fb2e
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-21 16:10:34 +0600

    create today, this week components.

---
+++ src/components/utilities/ThisWeek.tsx
@@ -0,0 +1,9 @@
+const ThisWeek = () => {
+  return (
+    <div className="h-full w-full pl-10 pt-10">
+      <h1 className="text-5xl font-bold text-slate-900">This Week</h1>
+    </div>
+  );
+};
+
+export default ThisWeek;
---
+++ src/components/utilities/Today.tsx
@@ -0,0 +1,9 @@
+const Today = () => {
+  return (
+    <div className="h-full w-full pl-10 pt-10">
+      <h1 className="text-5xl font-bold text-slate-900">Today</h1>
+    </div>
+  );
+};
+
+export default Today;
