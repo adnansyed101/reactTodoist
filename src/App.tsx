8e5b44e5f52f666fafadd69baba233824bbef808
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-25 17:17:44 +0600

    remove the pages system and create a tab system
    due to repetition of code made a tab system. Might also help when a project section in created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import InboxPage from "./components/Pages/InboxPage";
-import TodayPage from "./components/Pages/TodayPage";
-import ThisWeekPage from "./components/Pages/ThisWeekPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Inbox from "./components/Inbox";
+import Sidebar from "./components/Sidebar";
+import Today from "./components/Today";
+import ThisWeek from "./components/ThisWeek";
 import { useState } from "react";
-
 import uniqid from "uniqid";
 
 const App = (): JSX.Element => {
-  const [task, setTask] = useState([{ id: uniqid(), task: "learn react" }]);
+  const [tasks, setTasks] = useState<{}[]>([
+    { id: uniqid(), task: "learn react" },
+  ]);
+
+  const [tabId, setTabId] = useState<string>("inbox");
+
+  const toggleTabs = (id: string): void => {
+    setTabId(id);
+  };
+
   return (
-    <div className="App">
-      <InboxPage />
-      <TodayPage />
-      <ThisWeekPage />
+    <div className="grid h-screen w-screen grid-cols-[1fr_4fr] grid-rows-[80px_1fr_35px]">
+      <Header />
+      <Sidebar onTabClick={toggleTabs} />
+      {tabId === "inbox" && <Inbox />}
+      {tabId === "today" && <Today />}
+      {tabId === "thisWeek" && <ThisWeek />}
+      <Footer />
     </div>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,17 +2,21 @@ import { AiFillStar } from "react-icons/ai";
 import { IoToday } from "react-icons/io5";
 import { MdCalendarMonth } from "react-icons/md";
 
-const Sidebar = () => {
+type SidebarProps = {
+  onTabClick: (id: string) => void;
+};
+
+const Sidebar = ({ onTabClick }: SidebarProps) => {
   return (
     <div className="h-full w-full bg-slate-100 p-10">
-      <p className="sidebar-tab">
+      <p className="sidebar-tab" onClick={() => onTabClick("inbox")}>
         <AiFillStar />
         Inbox
       </p>
-      <p className="sidebar-tab">
+      <p className="sidebar-tab" onClick={() => onTabClick("today")}>
         <IoToday /> Today
       </p>
-      <p className="sidebar-tab">
+      <p className="sidebar-tab" onClick={() => onTabClick("thisWeek")}>
         <MdCalendarMonth /> This Week
       </p>
     </div>
