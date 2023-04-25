385bf1b0395eee302bbe8f59575e4345200d2050
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-25 16:38:17 +0600

    remove context system.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import InboxPage from "./components/Pages/InboxPage";
+import TodayPage from "./components/Pages/TodayPage";
+import ThisWeekPage from "./components/Pages/ThisWeekPage";
+import { useState } from "react";
+
+import uniqid from "uniqid";
 
 const App = (): JSX.Element => {
+  const [task, setTask] = useState([{ id: uniqid(), task: "learn react" }]);
   return (
     <div className="App">
       <InboxPage />
+      <TodayPage />
+      <ThisWeekPage />
     </div>
   );
 };
