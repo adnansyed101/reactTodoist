2a402f3f15259f4f09c8e0eed9b7c0cc0b8278b3
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-27 14:45:40 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="col-span-2 bg-slate-900 py-1 text-center text-slate-100">
-      Copyright © 2021 Adnan
+      Copyright © {new Date().getFullYear()} Adnan
     </footer>
   );
 };
