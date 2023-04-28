b47e0f5345b201bb971ef77d9b2d5a9844ad2666
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-28 19:03:29 +0600

    Create ShowTaskList.tsx

---
+++ src/components/ShowTaskList.tsx
@@ -0,0 +1,53 @@
+import { BsCheck2All, BsFillTrashFill } from "react-icons/bs";
+import { format, parseISO } from "date-fns";
+import TaskEdit from "./TaskEdit";
+import { AiOutlineEdit } from "react-icons/ai";
+import { useState } from "react";
+import Task from "../@types/Task";
+
+type ShowTaskListProps = {
+  task: Task;
+  toggleCompleted: (id: string) => void;
+  removeTask: (id: string) => void;
+};
+
+const ShowTaskList = ({
+  task,
+  toggleCompleted,
+  removeTask,
+}: ShowTaskListProps) => {
+  const [showEdit, setShowEdit] = useState(true);
+
+  const toggleEditBox = () => {
+    setShowEdit((prev) => !prev);
+  };
+
+  const content = showEdit ? (
+    <td>
+      <TaskEdit task={task}/>
+    </td>
+  ) : (
+    <td>{task.isCompleted ? <del>{task.task}</del> : task.task}</td>
+  );
+
+  return (
+    <tr
+      className="space-x-10 border-b-2 border-slate-500 py-1 text-xl"
+      key={task.id}
+    >
+      <td onClick={() => toggleCompleted(task.id)}>
+        <BsCheck2All />
+      </td>
+      {content}
+      <td>Date: {format(parseISO(task.date), "MM-dd-yyyy")}</td>
+      <td>
+        <BsFillTrashFill onClick={() => removeTask(task.id)} />
+      </td>
+      <td>
+        <AiOutlineEdit onClick={toggleEditBox} />
+      </td>
+    </tr>
+  );
+};
+
+export default ShowTaskList;
