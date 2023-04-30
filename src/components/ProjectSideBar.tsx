21f54a4f28b9aee06d45ace2ba988799255d99c9
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-30 22:41:09 +0600

    create add button for sideBar Projects

--- src/components/ProjectSideBar.tsx
+++ src/components/ProjectSideBar.tsx
@@ -1,20 +1,24 @@
+import { useState } from "react";
 import Project from "../@types/Project";
+import { AiOutlinePlus } from "react-icons/ai";
 
 type ProjectSideBarProps = {
   tabId: string;
-  project: Project;
+  projects: Project[];
   handleClick: (tab: string) => void;
 };
 
 const ProjectSideBar = ({
   tabId,
-  project,
+  projects,
   handleClick,
 }: ProjectSideBarProps) => {
-  return (
-    <div>
-      <h2 className="sidebar-project">Projects</h2>
+  const [inputBox, setInputBox] = useState(false);
+
+  const showProjects = projects.map((project: Project) => {
+    return (
       <p
+        key={project.id}
         className={
           tabId === project.title ? "sidebar-tab-border" : "sidebar-tab"
         }
@@ -22,6 +26,23 @@ const ProjectSideBar = ({
       >
         {project.title}
       </p>
+    );
+  });
+
+  return (
+    <div className="my-4">
+      <div className="flex justify-between">
+        <h2 className="sidebar-project">Projects</h2>
+        <button
+          type="button"
+          // onClick={toggleInputBox}
+          className="flex items-center justify-center gap-2 rounded-md bg-slate-900 px-3 py-2 text-slate-100 hover:bg-slate-950 hover:text-slate-50"
+        >
+          Add Task
+          <AiOutlinePlus />
+        </button>
+      </div>
+      {showProjects}
     </div>
   );
 };
