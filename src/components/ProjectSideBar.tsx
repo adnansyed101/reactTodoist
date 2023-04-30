3de02a89405ac9738ed42ef7e0f1d4b7a5db3fcb
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-04-30 22:45:29 +0600

    create toggle function for form element

--- src/components/ProjectSideBar.tsx
+++ src/components/ProjectSideBar.tsx
@@ -13,7 +13,11 @@ const ProjectSideBar = ({
   projects,
   handleClick,
 }: ProjectSideBarProps) => {
-  const [inputBox, setInputBox] = useState(false);
+  const [inputBox, setInputBox] = useState(true);
+
+  const toggleInputBox = () => {
+    setInputBox((prev) => !prev);
+  };
 
   const showProjects = projects.map((project: Project) => {
     return (
@@ -35,13 +39,44 @@ const ProjectSideBar = ({
         <h2 className="sidebar-project">Projects</h2>
         <button
           type="button"
-          // onClick={toggleInputBox}
+          onClick={toggleInputBox}
           className="flex items-center justify-center gap-2 rounded-md bg-slate-900 px-3 py-2 text-slate-100 hover:bg-slate-950 hover:text-slate-50"
         >
           Add Task
           <AiOutlinePlus />
         </button>
       </div>
+
+      {inputBox && (
+        <form
+          className="grid w-full grid-cols-2 gap-1 p-3"
+          // onSubmit={onInputSubmit}
+        >
+          <input
+            required
+            type="text"
+            // onChange={(e) => onChange(e)}
+            // value={taskObj.task}
+            name="task"
+            placeholder="Enter Task"
+            className="input-box col-span-2"
+          />
+          <button
+            type="submit"
+            className="green-button col-span-1 py-1 text-lg font-bold md:text-xl"
+          >
+            Add
+          </button>
+          <button
+            type="button"
+            className="red-button col-span-1 py-1 text-lg font-bold md:text-xl"
+            // onClick={cancelSubmit}
+          >
+            Cancel
+          </button>
+        </form>
+      )}
+
       {showProjects}
     </div>
   );
